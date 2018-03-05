cfb30fd16c8fffe6700929b851d600da13c1e8d3
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2018-03-05 16:06:30 -0700

    Add javadoc for FunctionRunnerSendable

--- src/com/teamcautionrobotics/misc2018/FunctionRunnerSendable.java
+++ src/com/teamcautionrobotics/misc2018/FunctionRunnerSendable.java
@@ -6,12 +6,19 @@ import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SendableBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
+/**
+ * Use this class to have a button on the SmartDashboard that will run a function.
+ * @author schuyler
+ *
+ */
 public class FunctionRunnerSendable extends SendableBase implements Sendable {
 
     private boolean needsToRun;
     private final BooleanSupplier function;
 
     /**
+     * The name will be the name on the SmartDashboard. The {@code function} is called after
+     * the button is clicked and continues to be called until it returns true.
      * 
      * @param name
      * @param function Returns true when complete.
