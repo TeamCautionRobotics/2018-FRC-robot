2f916f1deee4a8294184ad46dcf922fa2036fa01
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2018-03-05 22:49:07 -0700

    Remove unused function from Robot.java

--- src/com/teamcautionrobotics/robot2018/Robot.java
+++ src/com/teamcautionrobotics/robot2018/Robot.java
@@ -341,9 +341,6 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void testPeriodic() {}
-
-    @Override
     public void robotPeriodic() {
         liftEncoderResetSendable.update();
     }
